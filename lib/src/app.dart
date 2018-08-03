6074571d33bb6955b15bc8a5200d4c44042338aa
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-03 13:52:45 +0530

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'User Login',
+      home: Scaffold(
+        body: Text('Login Form Here!'),
+      ),
+    );
+  }
+
+}
