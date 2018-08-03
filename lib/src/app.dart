9fcb8aadc820c5de7e05a132751dae748a1fd459
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-03 15:15:18 +0530

    Basic Structure setup for Login Screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/login_screen.dart';
+
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -7,7 +9,7 @@ class App extends StatelessWidget{
     return MaterialApp(
       title: 'User Login',
       home: Scaffold(
-        body: Text('Login Form Here!'),
+        body: LoginScreen(),
       ),
     );
   }
