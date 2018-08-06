275b499982c1504826d7cd60060a374a6ad45007
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-06 14:55:24 +0530

    Fetch value from email and password field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,6 +10,8 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
 
   final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -31,50 +33,60 @@ class LoginScreenState extends State<LoginScreen> {
   }
 
 
-}
-
-
-Widget emailField() {
-  return TextFormField(
-    decoration: InputDecoration(
-      labelText: 'Email Address',
-      hintText: 'name@example.com',
-    ),
-    keyboardType: TextInputType.emailAddress,
-    validator: (String value) {
-      if (!value.contains('@')) {
-        return 'Please enter valid email';
-      }
-    },
-  );
-}
-
-Widget passwordField() {
-  return TextFormField(
-    decoration: InputDecoration(
-      labelText: 'Password',
-      hintText: 'Password',
-    ),
-    obscureText: true,
-    validator: (String value) {
-      if (value.length > 4 && value.length < 20) {
-        return 'Password should be between 4 and 20 characters';
-      }
-    },
-  );
-}
-
-Widget submitButton() {
-  return RaisedButton(
-      child: Text(
-          'Submit',
-          style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-          )
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'name@example.com',
       ),
-      color: Colors.red,
-      onPressed: () {
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (String value) {
+        email = value;
+      },
+      validator: (String value) {
+        if (!value.contains('@')) {
+          return 'Please enter valid email';
+        }
+      },
+    );
+  }
 
-      });
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+      obscureText: true,
+      onSaved: (String value) {
+        password = value;
+      },
+      validator: (String value) {
+        if (value.length > 4 && value.length < 20) {
+          return 'Password should be between 4 and 20 characters';
+        }
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+        child: Text(
+            'Submit',
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+            )
+        ),
+        color: Colors.red,
+        onPressed: () {
+          if (formKey.currentState.validate()) {
+            formKey.currentState.save();
+          }
+        });
+  }
+
 }
+
+
+
