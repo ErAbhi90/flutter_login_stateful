6b1ae4c32a78ed2edcf0fed2257a091862dd6cc6
ErAbhi90 <er.abhithakral@gmail.com>
2018-08-08 11:34:47 +0530

    Updated Validations using validator package

--- lib/src/mixins/validation_mixin.dart
+++ lib/src/mixins/validation_mixin.dart
@@ -1,16 +1,28 @@
+import 'package:validator/validator.dart';
+
 class ValidationMixin {
   String validateEmail(String value) {
-    if (value.length > 4 && value.length < 20) {
-      return 'Password should be between 4 and 20 characters';
+    if (!isEmail(value)) {
+      return 'Email is not valid';
     }
 
     return null;
   }
 
   String validatePassword(String value) {
-    if (value.length > 4 && value.length < 20) {
+    if (value.length < 4 || value.length > 20) {
       return 'Password should be between 4 and 20 characters';
     }
+    else if (isAlpha(value)) {
+      return 'Password must contain a number';
+    }
+    else if (isNumeric(value)) {
+      return 'Password must contain an character';
+    }
+    else if (isAlphanumeric(value)) {
+      return 'Password must contain a special character';
+    }
+
 
     return null;
   }
